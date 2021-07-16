17f11d3adee63d0fafa57d1542d42e8571f735f4
alireza7agm <66559901+alireza7agm@users.noreply.github.com>
2021-07-17 00:29:25 +0430

    Update HalfBandFFT.m

--- HalfBandFFT.m
+++ HalfBandFFT.m
@@ -1,10 +1,10 @@
-function [y,f] = HalfBandFFT(InputSignal)
+function [X,f] = HalfBandFFT(InputSignal)
 L=length(InputSignal);          % data length
 freq = 2*pi/L * linspace(0,L,L);     
 fftSignal = fft(InputSignal); 
 fftSignal = abs(fftSignal)/(L/2);     % amplitude spectrum   
 
-plot(freq,fftSignal);
+plot(freq,fftSignal, 'LineWidth' , 2);
 grid on; grid minor;
 title('Half Band FFT','Interpreter','LaTeX');
 xlabel('$\Omega$','Interpreter','LaTeX');
@@ -14,7 +14,7 @@ xlim([0 2*pi]);
 xticks(0:pi/4:2*pi);
 xticklabels({'0','\pi/4','\pi/2','3\pi/4','\pi','5\pi/4','3\pi/2','7\pi/4','2\pi'})
 
-y = fftSignal;
+X = fftSignal;
 f = freq;
 end
 
