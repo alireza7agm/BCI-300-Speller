e2662a56fc0bbfc2b31bb7c73b166e9b0d088536
alireza7agm <66559901+alireza7agm@users.noreply.github.com>
2021-07-16 04:48:38 +0430

    Delete CorrCoefMat.m

--- CorrCoefMat.m
+++
@@ -1,8 +0,0 @@
-function CorrMat = CorrCoefMat(data)
-    n = size(data,1);
-    X1 = sum(data.*data,2);
-    Xnorm = repmat(X1,1,n);
-    Ynorm = Xnorm.';
-
-    CorrMat = (data*data')./sqrt(Xnorm.*Ynorm);
-end
