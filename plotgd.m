403dc7f908b5d0298c101dd307d9387e667d56c5
Armin Panjehpour <62140916+ARMINPAN@users.noreply.github.com>
2021-07-17 17:07:04 +0430

    Update plotgd.m

--- plotgd.m
+++ plotgd.m
@@ -1,9 +1,13 @@
 function plotgd(h,N)
-    gd1 = groupdelay(h,N); % our functino
+    gd1 = groupdelay(h,N); % our function
     gd2 = grpdelay(h,1,N); % matlab function
     figure;
     subplot(2,1,1)
     plot(gd1)
+    grid on;
+    title('group delay calculated by our implementation','interpreter','Latex')
     subplot(2,1,2)
     plot(gd2)
+    title('group delay calculated by Matlab function/grpdelay','interpreter','Latex')
+    grid on;
 end
